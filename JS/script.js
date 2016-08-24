3938a91b327a604594673085d51c060ab10ca95d
HavocRazr30 <havocrazr30@gmail.com>
2016-08-23 20:03:37 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   $('p').fadeIn(2000);
   
   $('header').click(function(){
-    $(this).css('background-color','gray');
+    $('header').css('background-color','gray');
   });
   
   $('#fancy-text').click(function(){
