7f2621032e3a82be42311f9dd38911f98eea37e5
HavocRazr30 <havocrazr30@gmail.com>
2016-08-23 20:07:53 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   $('p').fadeIn(2000);
   
   $('header').click(function(){
-    $(this).css('background-image','img url="https://static.pexels.com/photos/69020/pexels-photo-69020-medium.jpeg"');
+    $(this).css('background-image','img url="https://static.pexels.com/photos/69020/pexels-photo-69020-medium.jpeg"', 'green');
   });
   
   $('#fancy-text').click(function(){
