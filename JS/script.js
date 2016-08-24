ca5dbc1ceb89704302d2562dbd1b8044a98228b2
HavocRazr30 <havocrazr30@gmail.com>
2016-08-23 19:55:59 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,6 @@ $(document).ready(function(){
   $('h1').fadeIn(5000);
   $('p').fadeIn(2000);
   $('header').click(function(){
-    $(this).css('background-image: url("https://static.pexels.com/photos/979/sand-summer-desert-car-large.jpg")','black');
+    $(this).css('background-image': url("https://static.pexels.com/photos/979/sand-summer-desert-car-large.jpg")','black');
   });
 });
