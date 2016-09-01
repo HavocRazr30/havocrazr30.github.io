883ce59728637cf537e6ec3ed7fbd5c0f24028e3
HavocRazr30 <havocrazr30@gmail.com>
2016-09-01 17:11:58 -0500

    Update orange.js

--- ethan-tesch-assessment/js/orange.js
+++ ethan-tesch-assessment/js/orange.js
@@ -1,16 +1,1 @@
 //jquery file
-$(document).ready(function() {
-   $('div').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
-   $('div').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       }); 
-   });
-   $('div').click(function() {
-       $(this).toggle(1000);
-   }); 
-});
